8d4de3967d4ac44e5471b0b38c79b081bdb7272e
Douglas Roesch <Douglas.Roesch@bnymellon.com>
2021-03-02 18:07:11 -0500

    Cleanup

--- src/test/java/org/deroesch/demodb/BasicJDBCTest.java
+++ src/test/java/org/deroesch/demodb/BasicJDBCTest.java
@@ -28,11 +28,89 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 class BasicJDBCTest {
 
+    /**
+     * Test a simple query.
+     * 
+     * @throws SQLException
+     */
+    @Test
+    void testPlainJDBC() throws SQLException {
+
+        log.info("-----------------------------------------------------");
+        log.info("Starting");
+        final String stmt = "SELECT id, owner_id, label, email_address FROM public.email_address;";
+        try (Connection conn = DriverManager.getConnection(url); PreparedStatement ps = conn.prepareStatement(stmt)) {
+            log.info("Got connection");
+
+            final ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int i = 1;
+                final int id = rs.getInt(i++);
+                final int ownerId = rs.getInt(i++);
+                final String label = rs.getString(i++);
+                final String address = rs.getString(i++);
+
+                log.info(String.format("%s, %s, %s, %s", id, ownerId, label, address));
+            }
+        }
+        log.info("Ending");
+        log.info("-----------------------------------------------------");
+    }
+
+    /**
+     * Test a more complex query read from a file.
+     * 
+     * @throws SQLException
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    @Test
+    void testBigQuery() throws SQLException, FileNotFoundException, IOException {
+
+        log.info("-----------------------------------------------------");
+        log.info("Starting");
+
+        String stmt = "empty";
+        final Resource resource = context.getResource(String.format("classpath:queries/getStreetAddresses.sql"));
+        try (BufferedInputStream bis = new BufferedInputStream(resource.getInputStream())) {
+
+            // Read the whole query from the file.
+            stmt = new String(bis.readAllBytes());
+        }
+
+        try (Connection conn = DriverManager.getConnection(url); PreparedStatement ps = conn.prepareStatement(stmt)) {
+            log.info("Got connection");
+
+            final ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int i = 1;
+                final String lastName = rs.getString(i++);
+                final String firstName = rs.getString(i++);
+                final String middleName = rs.getString(i++);
+                final String label = rs.getString(i++);
+                final String addr1 = rs.getString(i++);
+                final String addr2 = rs.getString(i++);
+                final String city = rs.getString(i++);
+                final String state = rs.getString(i++);
+                final int zip_code = rs.getInt(i++);
+
+                final String msg = String.format("%s %s %s %s %s %s %s %s %s", lastName, firstName, middleName, label,
+                        addr1, addr2, city, state, zip_code);
+                log.info(msg);
+            }
+        }
+        log.info("Ending");
+        log.info("-----------------------------------------------------");
+
+    }
+
     @Autowired
     private ApplicationContext context;
 
     //
-    // The @Value elements below come from application.properties
+    // The @Value elements below come from application.properties file
     //
 
     @Value("${db.host}")
@@ -55,91 +133,14 @@ class BasicJDBCTest {
 
     @BeforeEach
     void init() {
-
         //
-        // Have to do this initialization at the instance level or @Value fetches won't
-        // work.
+        // We have to do field initialization at the instance level or the @Value
+        // fetches don't work.
         //
         if (null == url) {
             final String str = "jdbc:postgresql://%s:%s/%s?user=%s&password=%s&ssl=false";
             url = String.format(str, host, port, database, user, password);
         }
-    }
-
-    /**
-     * Test a simple query.
-     * 
-     * @throws SQLException
-     */
-    @Test
-    void testPlainJDBC() throws SQLException {
-
-        final String stmt = "SELECT id, owner_id, label, email_address FROM public.email_address;";
-        log.info("-----------------------------------------------------");
-        log.info("Starting");
-        try (Connection conn = DriverManager.getConnection(url); PreparedStatement p = conn.prepareStatement(stmt)) {
-            log.info("Got connection");
-
-            final ResultSet results = p.executeQuery();
-
-            while (results.next()) {
-                final int id = results.getInt(1);
-                final int ownerId = results.getInt(2);
-                final String label = results.getString(3);
-                final String address = results.getString(4);
-
-                log.info(String.format("%s, %s, %s, %s", id, ownerId, label, address));
-            }
-        }
-        log.info("Ending");
-        log.info("-----------------------------------------------------");
-    }
-
-    /**
-     * Test a more complex query read from a file.
-     * 
-     * @throws SQLException
-     * @throws FileNotFoundException
-     * @throws IOException
-     */
-    @Test
-    void testBigQuery() throws SQLException, FileNotFoundException, IOException {
-
-        String stmt = "empty";
-        final Resource resource = context.getResource(String.format("classpath:queries/getStreetAddresses.sql"));
-        try (BufferedInputStream bis = new BufferedInputStream(resource.getInputStream())) {
-
-            // Read the whole query from the file.
-            stmt = new String(bis.readAllBytes());
-        }
-
-        log.info("-----------------------------------------------------");
-        log.info("Starting");
-        try (Connection conn = DriverManager.getConnection(url); PreparedStatement p = conn.prepareStatement(stmt)) {
-            log.info("Got connection");
-
-            final ResultSet results = p.executeQuery();
-
-            while (results.next()) {
-                int i = 1;
-                final String lastName = results.getString(i++);
-                final String firstName = results.getString(i++);
-                final String middleName = results.getString(i++);
-                final String label = results.getString(i++);
-                final String addr1 = results.getString(i++);
-                final String addr2 = results.getString(i++);
-                final String city = results.getString(i++);
-                final String state = results.getString(i++);
-                final int zip_code = results.getInt(i++);
-
-                final String msg = String.format("%s %s %s %s %s %s %s %s %s", lastName, firstName, middleName, label,
-                        addr1, addr2, city, state, zip_code);
-                log.info(msg);
-            }
-        }
-        log.info("Ending");
-        log.info("-----------------------------------------------------");
-
     }
 
 }
